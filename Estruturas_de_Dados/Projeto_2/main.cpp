b29224ad577b9628dfb9b9a1016e19a075bfcfb9
maykonmarcosjunior <maykon.marcos.junior.@grad.ufsc.br>
2023-06-15 17:07:42 -0300

    Versão Inicial

--- Estruturas_de_Dados/Projeto_2/main.cpp
+++ Estruturas_de_Dados/Projeto_2/main.cpp
@@ -2,23 +2,101 @@
 #include <fstream>
 #include <string.h>
 #include "trie.h"
+#include "ArrayList.h"
+#define MAX 100
+
+using namespace std;
+
+void le_arquivo(string filename,
+                structures::Trie *buscador,
+                structures::ArrayList<string> *saidas,
+                structures::ArrayList<string> *palavras) {
+    ifstream arquivo (filename);
+    string linha;
+    int checado[palavras->size()];
+    memset(checado, 0, palavras->size());
+    if (!arquivo.is_open()) {
+        cout << "arquivo inexistente";
+        return;
+    }
+    int start = -1, N, inicio;
+    while (getline(arquivo, linha)) {
+        string palavra;
+        N = linha.length();
+        bool continuar = false;
+        for (int i = 0; i < N; ++i) {
+            // contador de posições
+            start++;
+            if (continuar) {
+                continue;
+            }
+            if (linha[i] == '[')
+            {
+                inicio = start;
+            }
+            else if (linha[i] == '[')
+            {
+                int index = palavras->contains(palavra);
+                if (index == palavras->size()) {
+                    continue;
+                }
+                checado[index] = 1;
+                int prefixos = buscador->prefixos(palavra);
+                string saidaT = palavra + " is ";
+                if (prefixos) {
+                    saidaT += "prefix of " + static_cast<char>(prefixos);
+                    saidaT += " words\n";
+                    saidaT += palavra + "is at (";
+                    saidaT += static_cast<char>(inicio) + ',';
+                    // menos 1 é para ignorar a quebra de linha
+                    saidaT += static_cast<char>(N-1) + ")\n";
+                }
+                else {
+                    saidaT += "not a prefix\n";
+                }
+                saidas->insert(saidaT, index);
+                continuar = true;
+            }
+            else
+            {
+                palavra.push_back(linha[i]);
+            }
+        }
+    }
+    // para as entradas que não formem palavras
+    for (int i = 0; i < palavras->size(); ++i) {
+        if (!checado[i]) {
+            string saidaT = palavras->at(i);
+            int prefixos = buscador->prefixos(saidaT);
+            saidaT += " is ";
+            if (prefixos) {
+                saidaT += "prefix of " + static_cast<char>(prefixos);
+                saidaT += " words\n";
+            } else {
+                saidaT += "not a prefix\n";
+            }
+            saidas->insert(saidaT, i);
+        }
+    }
+}
 
 int main() {
     
-    using namespace std;
-    
     structures::Trie buscador = structures::Trie();
-    string filename;
-    string word;
-
-    cin >> filename;  // entrada
-    
-    while (1) {  // leitura das palavras ate' encontrar "0"
+    string filename, word;
+    structures::ArrayList<string> saidas{MAX}, palavras{MAX};
+    cin >> filename; // entrada
+    while (1) {  // leitura das palavras até encontrar "0"
         cin >> word;
         if (word.compare("0") == 0) {
             break;
         }
-        cout << word << endl;
+        buscador.insert(word);
+        palavras.push_back(word);
+    }
+    le_arquivo(filename, &buscador, &saidas, &palavras);
+    for (int i = 0; i < saidas.size(); ++i) {
+        cout << saidas[i];
     }
 
     return 0;
--- Estruturas_de_Dados/Projeto_2/trie.h
+++ Estruturas_de_Dados/Projeto_2/trie.h
@@ -1,7 +1,6 @@
 // Copyright [2023] <Maykon Marcos Junior>
 #include <string.h>
 #define L 26
-#define LIM 50
 
 namespace structures {
 
@@ -11,13 +10,11 @@ public:
     
     ~Trie();
 
-    void insert(const char& data);
+    void insert(std::string palavra);
 
-    void insert_str(const char palavra[]);
+    bool contains(std::string palavra) const;
 
-    void remove(const char& data);
-
-    bool contains(const char data[]) const;
+    bool prefixos(std::string palavra) const;
 
     bool empty() const;
 
@@ -30,18 +27,16 @@ private:
         Node* pai;
         int size;
         int posicao;
-        int prefixo;
+        int prefixos;
+        bool is_word;
 
         explicit Node(const char& newData, Node* NewPai) {
             letra = newData;
             pai = NewPai;
             size = 0;
-            posicao = 0;
-            prefixo = 0;
-            if (pai != nullptr) {
-                posicao = pai->posicao + 1; 
-                prefixo = pai->prefixo; 
-            }
+            is_word = false;
+            posicao = (pai == nullptr) ? 0 : pai->posicao + 1;
+            prefixos = (pai == nullptr)? 0 : pai->prefixos;
         }
 
         Node* find(const char& dado) {
@@ -65,15 +60,31 @@ private:
                 aux->insert(novo);
             }
         }
-        
-        bool contains(const char palavra[], char busca[], int pos) {
-            Node* teste = find(palavra[pos]);
+        void pref_update() {
+            prefixos++;
+            if (pai != nullptr) {
+                pai->pref_update();
+            }
+        }
+        bool contains(std::string palavra) {
+            if (posicao == static_cast<int>(palavra.length())) {
+                return true;
+            }
+            Node* teste = find(palavra[posicao]);
             if (teste != nullptr) {
-                busca[pos] = teste->letra;
-                pos++;
-                return teste->contains(palavra, busca, pos);
+                return teste->contains(palavra);
             }
             return false;
+        }
+        int prefixos(std::string palavra) {
+            if (posicao == static_cast<int>(palavra.length())) {
+                return prefixos;
+            }
+            Node* teste = find(palavra[posicao]);
+            if (teste != nullptr) {
+                return teste->contains(palavra);
+            }
+            return 0;
         }
     };
 
@@ -94,6 +105,7 @@ private:
 
 structures::Trie::Trie() {
     char temp = ' ';
+    // root nunca vai ser nullptr
     root = new Node(temp, nullptr);
     size_ = 0;
 }
@@ -101,40 +113,41 @@ structures::Trie::Trie() {
 structures::Trie::~Trie() {
     destructor(root);
 }
-
-void structures::Trie::insert(const char& data) {
-    Node *Novo = root, *pai = nullptr;
-    while (Novo != nullptr) {
-        pai = Novo;
-        Novo = pai->find(data);
-    }
-    Novo = new Node(data, pai);
-    pai->insert(Novo);
-    size_++;
-}
-
-void structures::Trie::insert_str(const char palavra[]) {
+void structures::Trie::insert(std::string palavra) {
     Node *pai = root, *aux = root->find(palavra[0]);
-    int i = 0, max = strlen(palavra);
+    int i = 0, max = static_cast<int>(palavra.length());
+    // verificando quais letras já estão lá
     while (aux != nullptr && i < max) {
         pai = aux;
         aux = pai->find(palavra[i]);
         i++;
     }
+    // significa que palavra é 
+    // um prefixo já inserido
     if (i == max) {
         return;
     }
-    for (;i < max; ++i) {
-        Node* Novo = new Node(palavra[i], pai);
+    // caso contrário
+    // deve-se inserir
+    // as letras restantes
+    Node *Novo;
+    for (; i < max; ++i) {
+        Novo = new Node(palavra[i],
+                        pai);
         pai->insert(Novo);
         pai = Novo;
         size_++;
     }
+    Novo->is_word = true;
+    Novo->pref_update();
 }
 
-bool structures::Trie::contains(const char data[]) const {
-    char busca[LIM];
-    return root->contains(data, busca, 0);
+bool structures::Trie::contains(std::string palavra) const {
+    return root->contains(palavra);
+}
+
+bool structures::Trie::contains(std::string palavra) const {
+    return root->prefixos(palavra);
 }
 
 bool structures::Trie::empty() const {
